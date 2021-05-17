8ee2fdf40b3e2c7eeb2e0e5cc65fffea56c6ffbb
chen605 <chow_chen_1996@hotmail.com>
2021-05-17 14:32:34 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from './firebase/firebase';
 import { login, logout, selectUser } from './features/userSlice';
 
 const App = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // logged out
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/">
-            <LandingPage />
-          </Route>
-        </Switch>
-      </div>
+      {!user ? (
+        <LandingPage />
+      ) : (
+        <>
+          {/* NAV */}
+          <Switch>
+            <Route exact path="/">
+              {/* HOMEPAGE */}
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </Route>
+          </Switch>
+        </>
+      )}
     </Router>
   );
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -40,7 +40,51 @@ const LandingPage = () => {
         alert(error.message);
       });
   };
-  return <div>HELLO</div>;
+  return (
+    <div>
+      {!signup ? (
+        <form>
+          <h1>Sign In</h1>
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <button type="submit" onClick={signIn}>
+            Sign In
+          </button>
+
+          <h4>
+            <span className="signupScreen__gray">Don't have an account? </span>
+            <span
+              className="signupScreen__link"
+              onClick={() => setSignup(!signup)}
+            >
+              Register now.
+            </span>
+          </h4>
+        </form>
+      ) : (
+        <form>
+          <h1>Sign Up</h1>
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <button type="submit" onClick={register}>
+            Sign Up
+          </button>
+
+          <h4>
+            <span className="signupScreen__gray">
+              Already have an account?{' '}
+            </span>
+            <span
+              className="signupScreen__link"
+              onClick={() => setSignup(!signup)}
+            >
+              Login to an existing account.
+            </span>
+          </h4>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default LandingPage;
