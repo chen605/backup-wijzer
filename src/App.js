062f8d7d360d9b2ba02e86960cc67a207499cbda
chen605 <chow_chen_1996@hotmail.com>
2021-05-17 13:57:02 +0200

    created app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { login, logout, selectUser } from './features/userSlice';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+require('dotenv').config();
+
+var config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
