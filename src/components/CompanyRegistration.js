24043a06f482a1ac9c991d7f9222d5986e3f4e43
Niek <n.veenstra02@gmail.com>
2021-05-19 11:43:36 +0200

    make small fix

--- src/components/CompanyRegistration.js
+++ src/components/CompanyRegistration.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../components/companyRegistrationFormStyles.css';
 
 const CompanyRegistration = () => {
   const [companyCredentials, setCompanyCredentials] = useState({
