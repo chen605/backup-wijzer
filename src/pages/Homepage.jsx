03ac46afe9cfc9b887a215711f73b44d0ee89fc6
chen605 <chow_chen_1996@hotmail.com>
2021-06-11 14:28:14 +0200

    cleaned up some code in all files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import UserRegistration from '../components/UserRegistration';
-import useFetch from '../custom-hooks/useFetch';
 
 const Homepage = () => {
-  const CRM = useFetch('/questions?domain=data');
-  console.log(CRM);
   return (
     <div>
       <UserRegistration />
